d1b25124a386acbbc1eeba6e3a4f402a0b009ed2
Jake Derrick <jakee2000jake@gmail.com>
2022-03-27 13:20:30 -0500

    add role to courses page

--- routes/courses.js
+++ routes/courses.js
@@ -3,17 +3,17 @@ const bodyParser = require('body-parser');
 const log = require('loglevel');
 const { isUserLoaded } = require('../services/auth');
 
-
 module.exports = function () {
   const router = express.Router();
   router.use(bodyParser.json());
-  
+
   router.get('/', isUserLoaded, async (req, res) => {
     res.render('layout', {
       pageTitle: 'Course Manager',
       group: 'courses',
       template: 'index',
       email: req.session.user.email,
+      role: req.session.user.role,
     });
     log.info(`${req.method} ${req.originalUrl} success: rendering course page`);
   });
