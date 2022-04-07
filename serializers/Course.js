933ddbdb0914ab6a465eb2fba0ed9e22bd8a45c8
Pirundy <pierce140he@gmail.com>
2022-04-07 04:15:13 -0500

    removed fake data for view

--- serializers/Course.js
+++ serializers/Course.js
@@ -7,7 +7,7 @@
 function deSerializeCourse(data) {
   return {
     id: data.id,
-    courseid: data.courseid,
+    courseid: data.courseid, // not implemented on the api
     name: data.name,
     credits: data.credits,
     section: data.section,
