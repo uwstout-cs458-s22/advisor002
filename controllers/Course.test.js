3c1a0e78965241b3ecceab179cd87e8b8fe67bd5
Pierce Heeschen <pierce140he@gmail.com>
2022-04-09 15:55:07 -0500

    Added more tests and fixed happy path
    Co-Authored-By: Ryan Hornberger <71733231+RHornberger20@users.noreply.github.com>

--- controllers/Course.test.js
+++ controllers/Course.test.js
@@ -16,22 +16,43 @@ describe('Course controller tests', () => {
 
   describe('fetchAll tests', () => {
     test('fetchAll - happy path test', async () => {
-      
       const testCourses = [
         {
-         id: 1,
-         name: "computers",
-         section: 1,
-         credits: 2,
-        }       
+          id: 1,
+          name: 'computers',
+          section: 1,
+          credits: 2,
+        },
       ];
-      axios.get.mockResolvedValueOnce({ courses: testCourses, status: 200 });
+      axios.get.mockResolvedValueOnce({ data: testCourses, status: 200 });
 
-      const result = await Course.findAll('mZAYn5aLEqKUlZ_Ad9U_fWr38GaAQ1oFAhT8ds245v7Q', "", 0, 100);
+      const result = await Course.findAll(
+        'mZAYn5aLEqKUlZ_Ad9U_fWr38GaAQ1oFAhT8ds245v7Q',
+        '',
+        0,
+        100
+      );
 
       expect(axios.get).toHaveBeenCalledWith('courses?criteria=');
       expect(result).toEqual(testCourses);
     });
+  });
+  test('findall -no records returned', async () => {
+    const courses = [];
+    axios.get.mockResolvedValueOnce({ status: 200, data: courses });
+    const result = await Course.findAll('mZAYn5aLEqKUlZ_Ad9U_fWr38GaAQ1oFAhT8ds245v7Q', '', 0, 100);
+    expect(axios.get).toHaveBeenCalledWith('courses?criteria=');
+    expect(result).toHaveLength(0);
+  });
 
+  test('findAll - error response', async () => {
+    axios.get.mockResolvedValueOnce({
+      status: 500,
+      data: { error: { status: 500, message: 'Internal Server Error' } },
+    });
+    await expect(
+      Course.findAll('mZAYn5aLEqKUlZ_Ad9U_fWr38GaAQ1oFAhT8ds245v7Q', '', 0, 100)
+    ).rejects.toThrow('Advisor API Error 500: Internal Server Error');
+    expect(axios.get).toHaveBeenCalledWith('courses?criteria=');
   });
 });
--- serializers/Course.js
+++ serializers/Course.js
@@ -7,7 +7,6 @@
 function deSerializeCourse(data) {
   return {
     id: data.id,
-    courseid: data.courseid, // not implemented on the api
     name: data.name,
     credits: data.credits,
     section: data.section,
