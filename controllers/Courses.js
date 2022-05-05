83548fa40737fca5cb88ca8738145b4561054284
Chris Kemp <zeewalnut@gmail.com>
2022-05-05 18:28:14 -0500

    Deletion Complete, no route tests working atm

--- controllers/Courses.js
+++ controllers/Courses.js
@@ -35,13 +35,10 @@ async function deleteCourse(sessionToken, courseId) {
   const request = axios.create({
     headers: { Authorization: `Bearer ${sessionToken}` },
   });
-  log.debug(courseId);
   const response = await request.delete(`courses/${courseId}`);
   if(response.status === 200) {
     return {
-      
       message: 'Course was deleted successfully',
-
       status: request.status
     };
   }
